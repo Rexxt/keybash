52bfd1261fdddbb3f6b16192646136052ca79776
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-09-17 21:02:13 +0200

    Prepared compiler and project init file

---
+++ main.py
@@ -0,0 +1,21 @@
+# Keybash compiler in Python 3
+# Follows Keybash 1.0.0
+from sys import argv
+import yaml
+
+def main(args):
+    # Project path
+    _project_path = args[1] # Note => args = sys.argv
+    _config_file = open(f"{_project_path}/keybash.yaml", 'r')
+    _config_file_contents = yaml.load(_config_file, Loader=yaml.FullLoader)
+    print("Overall")
+    for key, value in _config_file_contents.items():
+        print(f"{key}: {value}")
+    print("Project")
+    for key, value in _config_file_contents["Project"].items():
+        print(f"{key}: {value}")
+    print("Files")
+    for key, value in _config_file_contents["Files"].items():
+        print(f"{key}: {value}")
+
+main(argv)
